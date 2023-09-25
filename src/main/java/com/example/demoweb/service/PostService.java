139f70907c5384ae47b76c93b48985577e27c0be
Степан <@LAPTOP-B0PF4KGO>
2023-09-25 22:13:33 +0500

    added functionality for creating new posts

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -2,28 +2,23 @@ package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
-import java.util.Calendar;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+    private final List<Post> posts;
 
-    public List<Post> listAllPosts() {
-        var date1 = createDate(2023, 9, 25);
-        var date2 = createDate(2022, 10, 14);
-        var date3 = createDate(2023, 7, 23);
-        return List.of(new Post("e>)", date1),
-                new Post("Стёпа теперь джавист", date2),
-                new Post("Пост №3", date3));
+    public PostService() {
+        posts = new ArrayList<>();
     }
 
-    private Date createDate(int year, int month, int day) {
-        var calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DATE, day);
-        var date = calendar.getTime();
-        return Date.from(date.toInstant());
+    public List<Post> listAllPosts() {
+        return this.posts;
+    }
+
+    public void create(String text) {
+        this.posts.add(new Post(text, new Date()));
     }
 }
